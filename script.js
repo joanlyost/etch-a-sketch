e55f693a4add0cc56898e6ce0ca6b0fca9a69813
joancreates <135924201+joancreates@users.noreply.github.com>
2023-08-04 17:45:10 -0700

    Set drawing-board grid-template rows&column in JS
    Set drawing-board's rows and columns layout at the moment of size
    assignment in JS. This will allow to set these values by user input
    in the future.

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ function createSquares() {
     let width = 16;
     let height = 16;
     let totalSize = width*height;
-    drawingBoard.setAttribute("style", `width:${width}; height:${height};`);
+    drawingBoard.setAttribute
+        ("style", `width:${width}; height:${height}; 
+         grid-template-rows: repeat(${width}, 1px); 
+         grid-template-columns: repeat(${height}, 1px);`);
 
     for(let i = 0; i < totalSize; i++) {
         const div = document.createElement("div");
